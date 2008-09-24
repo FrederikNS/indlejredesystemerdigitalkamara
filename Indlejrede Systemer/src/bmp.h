f9f3db4306ccb3e1b8487f1f1813ad66b4d65161
FrederikNS <FrederikNS@6a76cbfc-8a2e-11dd-bc3c-d57f4316d18f>
2008-09-24 12:54:14 +0000

    Initial commit once ogoin

---
+++ Indlejrede Systemer/src/bmp.h
@@ -0,0 +1,54 @@
+
+/********************************************************************************
+ * BMP Header
+ ********************************************************************************/
+
+#ifndef __BMP_H
+#define __BMP_H
+
+#include "types.h"
+
+#pragma pack(push, 1)
+
+typedef struct {
+	HALFWORD 	BfType;  		    /* Must be 0x4D42 				  */
+	WORD	 	BfSize;				/* Size of the file in bytes 	  */
+	HALFWORD	BfReserved1;		/* Should be 0 					  */
+	HALFWORD	BfReserved2;		/* Should be 0 					  */
+	WORD		BfOffBits;			/* Offset of image data in file   */
+} BITMAPFILEHEADER;
+
+typedef struct {
+	WORD 		BiSize;				/* Size of this structure 		  */
+	WORD		BiWidth;			/* Width of the image in bytes    */
+	WORD		BiHeight;			/* Height of the image in bytes   */
+	HALFWORD	BiPlanes;			/* Should be 1 					  */
+	HALFWORD	BiBitCount;			/* Bit count (..) 				  */
+	WORD		BiCompression;		/* Compression used 			  */
+	WORD		BiSizeImage;		/* Size of the image in bytes 	  */
+	WORD		BiXPelsPerMeter; 	/* Pixels per meter, X			  */
+	WORD		BiYPelsPerMeter;	/* Pixels per meter, Y 			  */
+	WORD		BiClrUsed;			/* number of colors used 		  */
+	WORD		BiClrImportant;		/* number of important colors 	  */
+} BITMAPINFOHEADER;
+
+#pragma pack(pop)
+
+int bmp_ctgc(char *File, char *File2);
+
+/* Open and read the Header of a BMP file */
+int bmp_info_reader(char *File, BITMAPFILEHEADER *bmfh, BITMAPINFOHEADER *bmih);
+
+/* Open and read the image data of a BMP file */
+int bmp_image_reader(char *File, BITMAPFILEHEADER *bmfh, BITMAPINFOHEADER *bmih, BYTE *pixold);
+
+/* Builds a palette for a 256 grayscale BMP */
+int bmp_palette_creator(BYTE *pal);
+
+/* Converts the bmp image data to grayscale */
+int bmp_colour_to_grayscale(BITMAPFILEHEADER *bmfh, BITMAPINFOHEADER *bmih, BYTE *pixold, BYTE *pixnew);
+
+/* Store image to BMP file */
+int bmp_save(char *File, BITMAPFILEHEADER *bmfh, BITMAPINFOHEADER *bmih, BYTE *pixnew, BYTE *pal);
+
+#endif /* __BMP_H */
