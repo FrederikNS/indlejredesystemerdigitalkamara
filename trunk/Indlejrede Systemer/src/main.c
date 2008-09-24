d1ff90703aaee0806acdb73514207aebdb71b5ea
FrederikNS <FrederikNS@6a76cbfc-8a2e-11dd-bc3c-d57f4316d18f>
2008-09-24 12:54:14 +0000

    Initial commit once ogoin

---
+++ trunk/Indlejrede Systemer/src/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "bmp.h"
+
+int main (int argc, char *argv[]) {
+	char File[]="./example24.bmp";
+	char File2[]="./result.bmp";
+	bmp_ctgc(File,File2);
+
+    return 0;
+}
