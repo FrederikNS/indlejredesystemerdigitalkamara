d9825bf765a6bb3e511e8d5b091ac02d61333ecd
FrederikNS <FrederikNS@6a76cbfc-8a2e-11dd-bc3c-d57f4316d18f>
2008-10-19 15:34:52 +0000

    LOTS OF CHANGES IN THE ENTIRE STRUCTURE 2

---
+++ trunk/Indlejrede Systemer/src/ccd.c
@@ -0,0 +1,46 @@
+
+/*******
+ * CCD *
+ *******/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "bmp.h"
+
+static IMAGE current_image;
+
+WORD ccd_pixel_pointer;
+
+WORD ccd_get_height() {
+	return current_image.Height;
+}
+
+WORD ccd_get_width() {
+	return current_image.Width;
+}
+
+void ccd_capture_image() {
+	
+	if(!bmp_open("example24.bmp", &current_image)) {
+		printf("ccd_capture_image(): failed to open file\n");
+		exit(0);
+	}
+}
+
+void ccd_reset_pointer() {
+
+	/* reset */
+	ccd_pixel_pointer = 0;
+}
+
+BYTE ccd_get_pixel() {
+	return current_image.Pixels[ccd_pixel_pointer++];
+}
+
+WORD ccd_get_pixels() {
+	/* update pixel pointer */
+	ccd_pixel_pointer += 4;
+
+	return ((WORD)current_image.Pixels[ccd_pixel_pointer])<<8*3 || ((WORD)current_image.Pixels[ccd_pixel_pointer+1])<<8*2 || ((WORD)current_image.Pixels[ccd_pixel_pointer+2])<<8 || ((WORD)current_image.Pixels[ccd_pixel_pointer+4]);
+}
